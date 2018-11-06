72d813e344aa730290cb49591cd7df63a1af1b97
mo-12 <44545007+mo-12@users.noreply.github.com>
2018-11-06 17:00:53 +0100

    Update LSBnumpy.py

--- LSBnumpy.py
+++ LSBnumpy.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #im = Image.open("katze.jpg")
-im = Image.open(r"C:\Users\Mo\PycharmProjects\untitled2\venv\katze.jpg")
+im = Image.open(r"C:\Users\Mo\PycharmProjects\untitled2\venv\katze.jpg").convert("L")
+
+
 width,height = im.size;
 pixelMap = im.load();
-text = "Read the elements of a using this index order, and place the elements into the reshaped array using this index order. ‘C’ means to read / write the elements using C-like index order, with the last axis index changing fastest, back to the first axis index changing slowest. ‘F’ means to read / write the elements using Fortran-like index order, with the first index changing fastest, and the last index changing slowest. Note that the ‘C’ and ‘F’ options take no account of the memory layout of the underlying array, and only refer to the order of indexing. ‘A’ means to read / write the elements in Fortran-like index order if a is Fortran contiguous in memory, C-like order otherwise." # geheime Nachricht
+text = "abc"
 
 asciiText = []
 asciiTextBIN = []
@@ -35,25 +37,25 @@ for string in range(len(asciiTextBIN)):
 
 #convert image to numpy array
 numpyImage = np.array(im)
-#convert image to greyscale
-gray = np.mean(im, -1).astype(int)
 
 #create a new list that acts as a mask to encode the information
-mask = [int(i) for i in str(int(asciiString))]
+mask = [int(i) for i in str((asciiString))]
+
+print (mask)
 maskAnd = [i+254 for i in mask] #binary 11111110
 maskOr = mask
 #reshape the image from 2 dimensional [width,height] to one dimensional [width*height]
-flatImage = gray.flatten()
-fltcp = flatImage
+flatImage = numpyImage.flatten()
+
 #encode the information using binary operations
 #overwrite the zeros
 flatImage[0:maskAnd.__len__()] = np.bitwise_and(flatImage[0:maskAnd.__len__()],maskAnd)
 #overwrite the ones
 flatImage[0:maskOr.__len__()] = np.bitwise_or(flatImage[0:maskOr.__len__()],maskOr)
 
-result = flatImage.reshape(gray.shape)
+result = flatImage.reshape(numpyImage.shape)
 result = result.astype(np.uint8)
-plt.imshow(result-gray)
+plt.imshow(result-numpyImage)
 
 finalImage = Image.fromarray(result)
 
