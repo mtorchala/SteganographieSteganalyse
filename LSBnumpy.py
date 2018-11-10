297f0bc292d18bfd3cf38a38c177da0114322a11
Timo <5381765+timokae@users.noreply.github.com>
2018-11-10 14:14:18 +0100

    .

--- LSBnumpy.py
+++ LSBnumpy.py
@@ -1,22 +1,23 @@
 from PIL import Image
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 import numpy as np
 
-#im = Image.open("katze.jpg")
-im = Image.open(r"C:\Users\Mo\PycharmProjects\untitled2\venv\katze.jpg").convert("L")
+# im = Image.open("katze.jpg")
+im = Image.open("text_in_image/katze_gs.png")
 
 
-width,height = im.size;
-pixelMap = im.load();
+width, height = im.size
+pixelMap = im.load()
 text = "abc"
 
 asciiText = []
 asciiTextBIN = []
 for y in range(len(text)):
-    asciiText.append(ord(text[y]))              # Text in Binärform umwandeln
+    asciiText.append(ord(text[y]))  # Text in Binärform umwandeln
     asciiTextBIN.append(bin(ord(text[y]))[2:])
 
-#print("ASCII Text: " + str(asciiText))
+# print("ASCII Text: " + str(asciiText))
 
 for k in range(len(asciiTextBIN)):
     string = ""
@@ -24,39 +25,45 @@ for k in range(len(asciiTextBIN)):
         string += "0"
         asciiTextBIN[k] = string + asciiTextBIN[k]
 
-#print("ASCII Text BINÄR: " + str(asciiTextBIN))
+# print("ASCII Text BINÄR: " + str(asciiTextBIN))
 
 asciiString = ""
 for string in range(len(asciiTextBIN)):
-    asciiString += asciiTextBIN[string];
+    asciiString += asciiTextBIN[string]
 
-#print(asciiString)
-
-
+# print(asciiString)
 
 
-#convert image to numpy array
+# convert image to numpy array
 numpyImage = np.array(im)
 
-#create a new list that acts as a mask to encode the information
+# create a new list that acts as a mask to encode the information
 mask = [int(i) for i in str((asciiString))]
 
-#print (mask)
-maskAnd = [i+254 for i in mask] #binary 11111110
+# print (mask)
+maskAnd = [i + 254 for i in mask]  # binary 11111110
 maskOr = mask
-#reshape the image from 2 dimensional [width,height] to one dimensional [width*height]
+# reshape the image from 2 dimensional [width,height] to one dimensional [width*height]
 flatImage = numpyImage.flatten()
 
-#encode the information using binary operations
-#overwrite the zeros
-flatImage[0:maskAnd.__len__()] = np.bitwise_and(flatImage[0:maskAnd.__len__()],maskAnd)
-#overwrite the ones
-flatImage[0:maskOr.__len__()] = np.bitwise_or(flatImage[0:maskOr.__len__()],maskOr)
+# encode the information using binary operations
+# overwrite the zeros
+flatImage[0 : maskAnd.__len__()] = np.bitwise_and(
+    flatImage[0 : maskAnd.__len__()], maskAnd
+)
+# overwrite the ones
+flatImage[0 : maskOr.__len__()] = np.bitwise_or(flatImage[0 : maskOr.__len__()], maskOr)
 
 result = flatImage.reshape(numpyImage.shape)
 result = result.astype(np.uint8)
-plt.imshow(result-numpyImage)
+# plt.imshow(result-numpyImage)
 
 finalImage = Image.fromarray(result)
 
-finalImage.save("katzesteg.png","PNG", quality=100, optimize=False, progressive=False)
+finalImage.save(
+    "text_in_image/katze_encoded.png",
+    "PNG",
+    quality=100,
+    optimize=False,
+    progressive=False,
+)
---
+++ seminar-1.md
@@ -0,0 +1,17 @@
+# Seminar 1 - Ablauf
+- Einleitung (Felix, ~5 Minuten)
+  - Was ist Steganographie
+  - Geschichtliches
+  - Heutige Anwendungen
+
+- LSB Methode 
+  - Naiver Ansatz
+  - techn. Grundlagen Bilder (Moritz)
+  - Bildspeicherung und Kompression / Bedeutung für die Informationen (Moritz)
+  - Erklären was wir machen / Algorithmus (Timo)
+    -  Text in Ascii in Binärstream
+    -  Binärstream in einzelne Bits
+    -  einzelne Bits in das Bild einfügen
+    -  Verteilung von Bits
+
+- Ausblick
