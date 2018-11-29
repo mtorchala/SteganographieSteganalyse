f028b3fb7a2540443968de501f13f64fcfbddf77
mtorchala <marc.torchala@hs-bochum.de>
2018-11-29 23:44:13 +0100

    Histogramm-Angriff verbesserte grafische Darstellung

--- Steganalyse/Steganalyse_HistogrammAngriff.py
+++ Steganalyse/Steganalyse_HistogrammAngriff.py
@@ -1,6 +1,23 @@
 from PIL import Image
 import matplotlib.pyplot as plot
 
+img = Image.open("katze_encoded.png")
+img = img.convert('L')
+width, height = img.size
+pixel_map = img.load()
+
+encoded_img = Image.new(img.mode, img.size)
+X = []
+Y = []
+for pixel_value in range(256):
+    count = 0
+    for y in range(height):
+        for x in range(width):
+            if pixel_map[x, y] == pixel_value:
+                count += 1
+    X.append(pixel_value)
+    Y.append(count)
+plot.plot(X,Y)
 
 img = Image.open("katze_gs.png")
 img = img.convert('L')
@@ -18,7 +35,7 @@ for pixel_value in range(256):
                 count += 1
     X.append(pixel_value)
     Y.append(count)
-plot.plot(X,Y)
+plot.plot(X,Y,'C3')
 plot.xlabel("Pixelwert")
 plot.ylabel("Anzahl Pixel")
 plot.savefig('histogramm.png',dpi = 100);
