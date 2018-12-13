dd156695559d8f48110ee9a004322700838f6a7a
mtorchala <marc.torchala@hs-bochum.de>
2018-12-13 19:57:10 +0100

    Berechnung statistischer Merkmale

--- Steganalyse/Steganalyse_HistogrammAngriff.py
+++ Steganalyse/Steganalyse_HistogrammAngriff.py
@@ -5,10 +5,12 @@ img = Image.open("katze_encoded.png")
 img = img.convert('L')
 width, height = img.size
 pixel_map = img.load()
+anzahlpixel = width * height
+summepixelanzahl = 0
 
-encoded_img = Image.new(img.mode, img.size)
 X = []
 Y = []
+
 for pixel_value in range(256):
     count = 0
     for y in range(height):
@@ -19,14 +21,32 @@ for pixel_value in range(256):
     Y.append(count)
 plot.plot(X,Y)
 
+#Mittelwert für manipuliertes Bild bestimmen
+for ind in range(len(Y)):
+        summepixelanzahl = summepixelanzahl + Y[ind]
+
+mittelwert_manipuliert = summepixelanzahl / len(Y)
+print("Mittelwert(manipuliert): " + str(mittelwert_manipuliert))
+
+#Varianz für Originalbild bestimmen
+summe_abweichungen_manipuliert = 0;
+for ind in range(len(Y)):
+        summe_abweichungen_manipuliert = summe_abweichungen_manipuliert + (Y[ind] - mittelwert_manipuliert)**2
+
+varianz_manipuliert = summe_abweichungen_manipuliert / len(Y)
+print("Varianz(manipuliert): " + str(varianz_manipuliert))
+print("Standardabweichung(manipuliert): " + str(varianz_manipuliert**0.5))
+
 img = Image.open("katze_gs.png")
 img = img.convert('L')
 width, height = img.size
 pixel_map = img.load()
+anzahlpixel = width * height
+summepixelanzahl = 0
 
-encoded_img = Image.new(img.mode, img.size)
 X = []
 Y = []
+
 for pixel_value in range(256):
     count = 0
     for y in range(height):
@@ -35,6 +55,22 @@ for pixel_value in range(256):
                 count += 1
     X.append(pixel_value)
     Y.append(count)
+
+#Mittelwert für Originalbild bestimmen
+for ind in range(len(Y)):
+        summepixelanzahl = summepixelanzahl + Y[ind]
+
+mittelwert_original = summepixelanzahl / len(Y)
+print("Mittelwert(Original): " + str(mittelwert_original))
+
+#Varianz für Originalbild bestimmen
+summe_abweichungen_original = 0;
+for ind in range(len(Y)):
+        summe_abweichungen_original = summe_abweichungen_original + (Y[ind] - mittelwert_original)**2
+
+varianz_original = summe_abweichungen_original / len(Y)
+print("Varianz(Original): " + str(varianz_original))
+print("Standardabweichung(Original): " + str(varianz_original**0.5))
 plot.plot(X,Y,'C3')
 plot.xlabel("Pixelwert")
 plot.ylabel("Anzahl Pixel")
