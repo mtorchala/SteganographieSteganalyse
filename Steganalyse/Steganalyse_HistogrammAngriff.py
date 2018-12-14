9a31a08e138955f22f2b2158f5325c7f0c1d2a4c
mtorchala <marc.torchala@hs-bochum.de>
2018-12-14 18:43:09 +0100

    Subtraktion im Betrag

--- Steganalyse/Steganalyse_HistogrammAngriff.py
+++ Steganalyse/Steganalyse_HistogrammAngriff.py
@@ -9,7 +9,7 @@ anzahlpixel = width * height
 summepixelanzahl = 0
 
 X = []
-Y = []
+Ymanipuliert = []
 
 for pixel_value in range(256):
     count = 0
@@ -18,22 +18,22 @@ for pixel_value in range(256):
             if pixel_map[x, y] == pixel_value:
                 count += 1
     X.append(pixel_value)
-    Y.append(count)
-plot.plot(X,Y)
+    Ymanipuliert.append(count)
+plot.plot(X, Ymanipuliert)
 
 #Mittelwert für manipuliertes Bild bestimmen
-for ind in range(len(Y)):
-        summepixelanzahl = summepixelanzahl + Y[ind]
+for ind in range(len(Ymanipuliert)):
+        summepixelanzahl = summepixelanzahl + Ymanipuliert[ind]
 
-mittelwert_manipuliert = summepixelanzahl / len(Y)
+mittelwert_manipuliert = summepixelanzahl / len(Ymanipuliert)
 print("Mittelwert(manipuliert): " + str(mittelwert_manipuliert))
 
 #Varianz für Originalbild bestimmen
 summe_abweichungen_manipuliert = 0;
-for ind in range(len(Y)):
-        summe_abweichungen_manipuliert = summe_abweichungen_manipuliert + (Y[ind] - mittelwert_manipuliert)**2
+for ind in range(len(Ymanipuliert)):
+        summe_abweichungen_manipuliert = summe_abweichungen_manipuliert + (Ymanipuliert[ind] - mittelwert_manipuliert) ** 2
 
-varianz_manipuliert = summe_abweichungen_manipuliert / len(Y)
+varianz_manipuliert = summe_abweichungen_manipuliert / len(Ymanipuliert)
 print("Varianz(manipuliert): " + str(varianz_manipuliert))
 print("Standardabweichung(manipuliert): " + str(varianz_manipuliert**0.5))
 
@@ -45,7 +45,7 @@ anzahlpixel = width * height
 summepixelanzahl = 0
 
 X = []
-Y = []
+Yoriginal = []
 
 for pixel_value in range(256):
     count = 0
@@ -54,24 +54,34 @@ for pixel_value in range(256):
             if pixel_map[x, y] == pixel_value:
                 count += 1
     X.append(pixel_value)
-    Y.append(count)
+    Yoriginal.append(count)
 
 #Mittelwert für Originalbild bestimmen
-for ind in range(len(Y)):
-        summepixelanzahl = summepixelanzahl + Y[ind]
+for ind in range(len(Yoriginal)):
+        summepixelanzahl = summepixelanzahl + Yoriginal[ind]
 
-mittelwert_original = summepixelanzahl / len(Y)
+mittelwert_original = summepixelanzahl / len(Yoriginal)
 print("Mittelwert(Original): " + str(mittelwert_original))
 
 #Varianz für Originalbild bestimmen
 summe_abweichungen_original = 0;
-for ind in range(len(Y)):
-        summe_abweichungen_original = summe_abweichungen_original + (Y[ind] - mittelwert_original)**2
+for ind in range(len(Yoriginal)):
+        summe_abweichungen_original = summe_abweichungen_original + (Yoriginal[ind] - mittelwert_original) ** 2
 
-varianz_original = summe_abweichungen_original / len(Y)
+varianz_original = summe_abweichungen_original / len(Yoriginal)
 print("Varianz(Original): " + str(varianz_original))
 print("Standardabweichung(Original): " + str(varianz_original**0.5))
-plot.plot(X,Y,'C3')
+plot.plot(X, Yoriginal, 'C3')
+
+#Subtraktion von Original und manipuliertem Bild im Betrag
+Ysubbetrag = []
+for ind in range(len(Yoriginal)):
+        sub = Yoriginal[ind] - Ymanipuliert[ind]
+        sub = pow(sub,2)
+        Ysubbetrag.append(sub**0.5)
+plot.plot(X,Ysubbetrag,'C7')
+
+
 plot.xlabel("Pixelwert")
 plot.ylabel("Anzahl Pixel")
 plot.savefig('histogramm.png',dpi = 100);
